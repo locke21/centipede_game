ccedde959ee23582038e455662d78f41d7c07745
Loc Truong <100035903+locke21@users.noreply.github.com>
2022-07-23 22:40:51 -0400

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,52 +1,55 @@
 from turtle import Turtle
 
-MOVE_DISTANCE = 20
-UP = 90
-DOWN = 270
-LEFT = 180
-RIGHT = 0
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+centipede_movement = 20
+
 
 class Snake:
-
     def __init__(self):
         self.segments = []
-        self.create_snake()
+        self.make_segment()
         self.head = self.segments[0]
 
-    def create_snake(self):
-        for snake_body in range(3):
-            initial_snake = Turtle()
-            initial_snake.penup()
-            initial_snake.shape("square")
-            initial_snake.color("white")
-            initial_snake.setposition((0 - (snake_body * 20)), 0)
-            self.segments.append(initial_snake)
-
-    def add_segment(self):
-
-
-    def extend(self):
-
-
-    def move(self):
-        for segment_num in range(len(self.segments) -1, 0, -1):
-            new_x = self.segments[segment_num - 1].xcor()
-            new_y = self.segments[segment_num - 1].ycor()
-            self.segments[segment_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
-
     def up(self):
-        if self.head.heading() != DOWN:
-            self.head.setheading(UP)
+        if self.head.heading() != 270:
+            self.head.setheading(90)
 
     def down(self):
-        if self.head.heading() != UP:
-            self.head.setheading(DOWN)
-
-    def left(self):
-        if self.head.heading() != RIGHT:
-            self.head.setheading(LEFT)
+        if self.head.heading() != 90:
+            self.head.setheading(270)
 
     def right(self):
-        if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def make_segment(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        snake_segment = Turtle(shape='square')
+        snake_segment.penup()
+        snake_segment.color('white')
+        snake_segment.goto(position)
+        self.segments.append(snake_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def snake_movement(self):
+        for seg in range(len(self.segments) - 1, 0, -1):
+            last_position = self.segments[seg - 1].xcor(), self.segments[seg - 1].ycor()
+            self.segments[seg].goto(last_position)
+        self.segments[0].forward(centipede_movement)
+
+    def game_over(self):
+        for seg in self.segments:
+            seg.goto(800, 800)
+        self.segments.clear()
+        self.make_segment()
+        self.head = self.segments[0]
